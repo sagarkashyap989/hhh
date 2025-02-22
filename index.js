de46877b541452fd030c59cb3be119f3ca44d75a
sagarkashyap989 <saga11kashyap@mail.com>
2025-02-22 13:55:08 +0530

    configure package.jscon

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3001;
 const DATA_FILE = "hiraganaData.json";
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+    fs.readFile(DATA_FILE, "utf8", (err, data) => { 
+        return res.status(200).json({ message: "runingg..." }); 
+  });
+
 
 // GET endpoint - Fetch flashcards
 app.get("/flashcards", (req, res) => {
@@ -42,6 +49,6 @@ app.post("/flashcards", (req, res) => {
   });
 });
 
-app.listen(3001, () => {
-  console.log(`Server is running on http://localhost:${3001}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
